2f7f97cbbb4e07a293fd435cd4496c24578b15a7
m4ym4y <m4ym4y@users.noreply.github.com>
2025-09-29 14:46:28 -0700

    better tank colliders

--- src/Tank.tsx
+++ src/Tank.tsx
@@ -1,9 +1,15 @@
 import { useGLTF } from '@react-three/drei'
-import { RigidBody } from '@react-three/rapier'
+import { CuboidCollider, RigidBody } from '@react-three/rapier'
 
 function Tank() {
   const { scene } = useGLTF('/models/tank.glb')
-  return <RigidBody type="fixed" colliders="trimesh">
+  return <RigidBody type="fixed" colliders={false}>
+    <CuboidCollider args={[1, 10, 10]} position={[-11, 0, 0 ]} />
+    <CuboidCollider args={[1, 10, 10]} position={[11, 0, 0 ]} />
+    <CuboidCollider args={[12, 10, 1]} position={[0, 0, -11 ]} />
+    <CuboidCollider args={[12, 10, 1]} position={[0, 0, 11 ]} />
+    <CuboidCollider args={[12, 1, 12]} position={[0, 11, 0 ]} />
+    <CuboidCollider args={[12, 1, 12]} position={[0, -10, 0 ]} />
     <primitive object={scene} />
   </RigidBody>
 }
