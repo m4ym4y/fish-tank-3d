feef0005544ba29905b0693893c6aefeaa84bcce
m4ym4y <m4ym4y@users.noreply.github.com>
2025-10-03 17:42:23 -0700

    implement copy button in embed tool

--- src/overlay/EmbedTools.tsx
+++ src/overlay/EmbedTools.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './EmbedTools.css'
 import { useAppSelector } from '../state/hooks.ts'
 import * as util from '../util'
@@ -6,6 +6,8 @@ import queryString from 'query-string'
 
 function EmbedTools() {
   const [ popupVisible, setPopupVisible ] = useState(false)
+  const [ copied, setCopied ] = useState(false)
+
   const arrangement = useAppSelector(state => state.editor.arrangement)
 
   const serialized = util.serializeArrangement(arrangement)
@@ -15,17 +17,31 @@ function EmbedTools() {
   qParams.arrangement = serialized
   qParams.view = 'true'
   url.search = queryString.stringify(qParams)
+  const embedText = `<iframe width="400" height="300" src="${url.href}" />`
+
+  const copyEmbedCode = async () => {
+    await navigator.clipboard.writeText(embedText)
+    setCopied(true)
+  }
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setCopied(false)
+    }, 1000)
+
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [ copied ])
 
   return <div className="overlay-popup-tool embed-popup-button">
     <div className={`embed-popup ${popupVisible ? 'show' : ''}`} >
       <div className="embed-popup-tools">
-        <button>Copy to Clipboard</button>
+        <button onClick={copyEmbedCode}>{copied ? 'Copied!' : 'Copy to Clipboard'}</button>
         <div style={{ flexGrow: 1 }} />
         <button onClick={() => setPopupVisible(false)}>X</button>
       </div>
-      <textarea readOnly>
-        {`<iframe width="400" height="300" src="${url.href}" />`}
-      </textarea>
+      <textarea readOnly>{embedText}</textarea>
     </div>
     <button onClick={() => setPopupVisible(!popupVisible)}>Embed</button>
   </div>
